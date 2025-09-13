3775a60841df87bd2e481830be8bc92e4e5f8637
James Cape <jamescape777@pm.me>
2025-09-13 03:32:05 +0000

    build: add action-validator, cargo-deny to pre-commit

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -5,12 +5,13 @@ ln -sf /usr/local/cargo/bin /workspaces/iso3166/.cache/cargo/
 
 RUSTC_WRAPPER_save="$RUSTC_WRAPPER"
 unset RUSTC_WRAPPER
+cargo binstall -q -y --force sccache
+export RUSTC_WRAPPER="$RUSTC_WRAPPER_save"
+
+cargo binstall -q -y --force action-validator
 cargo binstall -q -y --force cargo-deny
 cargo binstall -q -y --force cargo-semver-checks
 cargo binstall -q -y --force release-plz
-cargo binstall -q -y --force action-validator
-cargo binstall -q -y --force sccache
-export RUSTC_WRAPPER="$RUSTC_WRAPPER_save"
 
 pushd /workspaces/iso3166 >/dev/null
 pre-commit install >/dev/null
