1d7626788aa496c3401c1b0ca179de25e122b39c
James Cape <jamescape777@pm.me>
2025-12-21 14:37:06 +0000

    docs: Move tests to README.md

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -125,40 +125,6 @@ fn m49() -> Result<TokenStream> {
         }
 
         /// ISO 3166 Countries.
-        ///
-        /// # Examples
-        ///
-        /// ```
-        /// use iso3166_static::Country;
-        ///
-        /// let country1 = Country::try_from_alpha2("US").expect("alpha2");
-        /// let country2 = Country::try_from_alpha3("USA").expect("alpha3");
-        ///
-        /// assert_eq!(country1, country2);
-        /// ```
-        ///
-        /// The [`FromStr`](::core::str::FromStr) implementation is more forgiving, as it will
-        /// remove any leading/trailing whitespace, and converting to an upper-case string.
-        ///
-        /// ```
-        /// use core::str::FromStr;
-        /// use iso3166_static::Country;
-        ///
-        /// let country1 = Country::from_str("  US  ").expect("trimmed ascii");
-        /// let country2 = Country::from_str("usa").expect("case insenitive");
-        ///
-        /// assert_eq!(country1, country2);
-        /// ```
-        ///
-        /// Some failure conditions:
-        /// ```
-        /// use core::str::FromStr;
-        /// use iso3166_static::Country;
-        ///
-        /// let _ = Country::try_from_alpha2("us").expect_err("not uppercase");
-        /// let _ = Country::try_from_alpha3("usa").expect_err("not uppercase");
-        /// let _ = Country::from_str("asdf").expect_err("not a code");
-        /// ```
         #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
         #[repr(u16)]
         pub enum Country {
