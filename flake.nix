384cf279ebd8df8022125d2342dce8936549efd8
James Cape <jamescape777@pm.me>
2026-06-19 20:49:45 -0400

    chore: setup nix environment
    - Add .envrc to drive via nix-direnv
    - Install relevant tools
    - Add pre-commit hooks

---
+++ flake.nix
@@ -0,0 +1,76 @@
+{
+  description = "A nix environment for the ISO 3166 crate family.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        overrides = builtins.fromTOML (builtins.readFile (self + "/rust-toolchain.toml"));
+      in
+      {
+        devShells.default = pkgs.mkShell rec {
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = with pkgs; [
+            bashInteractive
+            cargo-nextest
+            clang
+            llvmPackages.bintools
+            markdownlint-cli2
+            nixfmt
+            prek
+            rustup
+            statix
+          ];
+
+          RUSTC_VERSION = overrides.toolchain.channel;
+
+          # https://github.com/rust-lang/rust-bindgen#environment-variables
+          LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+
+          shellHook = ''
+            export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+            export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+            export BINSTALL_DISABLE_TELEMETRY=true
+
+            rustup toolchain install 1.88.0
+            rustup toolchain install nightly
+          '';
+
+          # Add precompiled library to rustc search path
+          RUSTFLAGS = builtins.map (a: "-L ${a}/lib") [
+            # add libraries here (e.g. pkgs.libvmi)
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
+
+          # Add glibc, clang, glib, and other headers to bindgen search path
+          BINDGEN_EXTRA_CLANG_ARGS =
+            # Includes normal include path
+            (builtins.map (a: ''-I"${a}/include"'') [
+              # add dev libraries here (e.g. pkgs.libvmi.dev)
+              pkgs.glibc.dev
+            ])
+            # Includes with special directory paths
+            ++ [
+              ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+            ];
+        };
+      }
+    );
+}
